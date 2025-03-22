632f849cd2b38dc9750c406dd6ca3f5672d7e498
Roger Sherman <rsherman@linux.com>
2025-03-22 13:22:31 +1000

    Add Cluster Autoscaler IAM policy and role

---
+++ terraform/modules/iam/cluster-autoscaler-policy.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name        = "${var.project}-${var.environment}-cluster-autoscaler"
+  description = "IAM policy for Cluster Autoscaler"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ]
+        Resource = ["*"]
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "cluster_autoscaler" {
+  name = "${var.project}-${var.environment}-cluster-autoscaler"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = var.oidc_provider_arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${var.oidc_provider}:sub" = "system:serviceaccount:kube-system:cluster-autoscaler"
+            "${var.oidc_provider}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  policy_arn = aws_iam_policy.cluster_autoscaler.arn
+  role       = aws_iam_role.cluster_autoscaler.name
+}
+
+output "cluster_autoscaler_role_arn" {
+  description = "ARN of the IAM role for the Cluster Autoscaler"
+  value       = aws_iam_role.cluster_autoscaler.arn
+} 
