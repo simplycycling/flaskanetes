53c549dc7bdc1540921a739c4894fd1706796bfd
Roger Sherman <rsherman@linux.com>
2025-03-22 12:52:18 +1000

    terraform: add OIDC provider resource
    - Add OIDC provider resource to EKS module\n- Add TLS provider for OIDC certificate

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -123,4 +123,21 @@ resource "aws_eks_node_group" "this" {
     },
     var.tags
   )
+}
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "main" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-eks-oidc"
+    },
+    var.tags
+  )
 } 
