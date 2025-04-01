2be0cf6189741507734257d7f522e4974b44a6a8
Roger Sherman <rsherman@linux.com>
2025-04-01 17:39:28 +1000

    refactor(vpc): rename 'this' resources to 'main' [skip ci]
    - Rename aws_vpc.this to aws_vpc.main
    
    - Rename aws_internet_gateway.this to aws_internet_gateway.main
    
    - Rename aws_nat_gateway.this to aws_nat_gateway.main

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "this" {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -14,7 +14,7 @@ resource "aws_vpc" "this" {
 # Public subnets
 resource "aws_subnet" "public" {
   count             = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
 
@@ -32,7 +32,7 @@ resource "aws_subnet" "public" {
 # Private subnets
 resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
 
@@ -45,8 +45,8 @@ resource "aws_subnet" "private" {
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 
   tags = merge(
     {
@@ -68,7 +68,7 @@ resource "aws_eip" "nat" {
 }
 
 # NAT Gateway
-resource "aws_nat_gateway" "this" {
+resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id  # Place NAT Gateway in first public subnet
 
@@ -82,11 +82,11 @@ resource "aws_nat_gateway" "this" {
 
 # Route tables
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
+    gateway_id = aws_internet_gateway.main.id
   }
 
   tags = merge(
@@ -98,11 +98,11 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
+    nat_gateway_id = aws_nat_gateway.main.id
   }
 
   tags = merge(
@@ -129,7 +129,7 @@ resource "aws_route_table_association" "private" {
 # Security group for EKS cluster
 resource "aws_security_group" "eks_cluster" {
   name_prefix = "${var.project}-${var.environment}-eks-cluster"
-  vpc_id      = aws_vpc.this.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 443
@@ -149,7 +149,7 @@ resource "aws_security_group" "eks_cluster" {
 # Security group for EKS nodes
 resource "aws_security_group" "eks_nodes" {
   name_prefix = "${var.project}-${var.environment}-eks-nodes"
-  vpc_id      = aws_vpc.this.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port       = 0
